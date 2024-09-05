fab5f7f87e39cae7fdd2de113948a6d41278b6e5
Miraj Yafi <mbyafi7@outlook.com>
2024-09-05 18:31:45 -0400

    setup login feature

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,20 @@ from config import app, db
 from models import UserAccount
 
 
+@app.route('/create_user', methods=["GET", "POST"])
+def create_user():
+    if request.method == "POST":
+        #post is when data from the client is sent over to the server
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        #request.form['example'] retrieves the value associated with the form field with the 'example attribute'
 
+        new_user = UserAccount(username=username, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+    
+    return redirect(url_for('signup'))
 
 @app.route('/')
 # the above url '/' is the base url for the website
